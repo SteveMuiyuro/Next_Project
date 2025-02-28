851ea1056f1a14c8c76af7a9b4f32c2267b76842
SteveMuiyuro <stevemuiyuro@gmail.com>
2025-02-28 13:45:18 +0300

    Revise code

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6GQ3wTrYvkIHGS6Gn8uj5oLYqFw+tNmBK7+sWIuYxSQ=\"\n}"
---
+++ .next/static/chunks/app_page_tsx_d9ebe370._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_d9ebe370._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_d9ebe370._.js",
+  "chunks": [
+    "static/chunks/app_ui_home_module_6dd21efa.css",
+    "static/chunks/35235_next_dist_78c4c20e._.js"
+  ],
+  "source": "dynamic"
+});
